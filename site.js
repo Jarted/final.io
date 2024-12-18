57530426137265f89e02ca4461bf2ccf906dfd09
Jarted <53982359+Jarted@users.noreply.github.com>
2024-12-18 17:38:31 -0500

    Add files via upload

---
+++ site.js
@@ -0,0 +1,4 @@
+document.getElementById('shop-now').addEventListener('click', function () {
+    const productsSection = document.getElementById('products');
+    productsSection.scrollIntoView({ behavior: 'smooth' });
+});
